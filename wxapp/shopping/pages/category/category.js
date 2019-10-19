d3f1cb8f0f2e00ddfd4a738b19c91bf0a156e02f
1131446340aa <1131446340@qq.com>
2019-10-19 16:40:44 +0800

    小程序增加购物车部分功能

--- wxapp/shopping/pages/category/category.js
+++ wxapp/shopping/pages/category/category.js
@@ -14,12 +14,157 @@ Page({
       { name: '淡饭', id: 'danfan' }
     ],
     curIndex: 0,
-    toView: 'guowei'
+    toView: 'guowei',
+   
+    detail: [
+      {
+        banner: 'http://img.redocn.com/sheji/20151221/tangguobudingbannerlingshipaipsdfencengsucai_5626296.jpg',
+        cate: '果味',
+        id: 'guowei',
+        detail: [
+          {
+            thumb: '../../image/c3.png',
+            name: '坚果'
+          },
+          {
+            thumb: '../../image/c1.png',
+            name: '瓜子'
+          },
+          {
+            thumb: '../../image/c2.png',
+            name: '大米'
+          },
+          {
+            thumb: '../../image/c4.png',
+            name: '芹菜'
+          }
+        ]
+      },
+      {
+        banner: 'http://img.redocn.com/sheji/20151221/tangguobudingbannerlingshipaipsdfencengsucai_5626296.jpg',
+        cate: '蔬菜',
+        id: 'shucai',
+        detail: [
+          {
+            thumb: '../../image/c3.png',
+            name: '坚果'
+          },
+          {
+            thumb: '../../image/c1.png',
+            name: '瓜子'
+          },
+          {
+            thumb: '../../image/c2.png',
+            name: '大米'
+          },
+          {
+            thumb: '../../image/c4.png',
+            name: '芹菜'
+          }
+        ]
+      },
+      {
+        banner: 'http://img.redocn.com/sheji/20151221/tangguobudingbannerlingshipaipsdfencengsucai_5626296.jpg',
+        cate: '炒货',
+        id: 'chaohuo',
+        detail: [
+          {
+            thumb: '../../image/c3.png',
+            name: '坚果'
+          },
+          {
+            thumb: '../../image/c1.png',
+            name: '瓜子'
+          },
+          {
+            thumb: '../../image/c2.png',
+            name: '大米'
+          },
+          {
+            thumb: '../../image/c4.png',
+            name: '芹菜'
+          }
+        ]
+      },
+      {
+        banner: 'http://img.redocn.com/sheji/20151221/tangguobudingbannerlingshipaipsdfencengsucai_5626296.jpg',
+        cate: '点心',
+        id: 'dianxin',
+        detail: [
+          {
+            thumb: '../../image/c3.png',
+            name: '坚果'
+          },
+          {
+            thumb: '../../image/c1.png',
+            name: '瓜子'
+          },
+          {
+            thumb: '../../image/c2.png',
+            name: '大米'
+          },
+          {
+            thumb: '../../image/c4.png',
+            name: '芹菜'
+          }
+        ]
+      },
+      {
+        banner: 'http://img.redocn.com/sheji/20151221/tangguobudingbannerlingshipaipsdfencengsucai_5626296.jpg',
+        cate: '粗茶',
+        id: 'cucha',
+        detail: [
+          {
+            thumb: '../../image/c3.png',
+            name: '坚果'
+          },
+          {
+            thumb: '../../image/c1.png',
+            name: '瓜子'
+          },
+          {
+            thumb: '../../image/c2.png',
+            name: '大米'
+          },
+          {
+            thumb: '../../image/c4.png',
+            name: '芹菜'
+          }
+        ]
+      },
+      {
+        banner: 'http://img.redocn.com/sheji/20151221/tangguobudingbannerlingshipaipsdfencengsucai_5626296.jpg',
+        cate: '淡饭',
+        id: 'danfan',
+        detail: [
+          {
+            thumb: '../../image/c3.png',
+            name: '坚果'
+          },
+          {
+            thumb: '../../image/c1.png',
+            name: '瓜子'
+          },
+          {
+            thumb: '../../image/c2.png',
+            name: '大米'
+          },
+          {
+            thumb: '../../image/c4.png',
+            name: '芹菜'
+          }
+        ]
+      }
+    ]
+
+
+
   },
   switchTab(e) {
-    console.log(e)
+    console.log(e.currentTarget.dataset.id)
     this.setData({
-      curIndex: e.currentTarget.dataset.index
+      curIndex: e.currentTarget.dataset.index,
+      toView:e.currentTarget.dataset.id
     })
   },
   /**
