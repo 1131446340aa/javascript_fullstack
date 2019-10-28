2f8bb24220dcd091d64b4fcf757d7947f0709854
1131446340aa <1131446340@qq.com>
2019-10-28 22:31:32 +0800

    es6

---
+++ es6/构造函数和原型/assign/demo1.js
@@ -0,0 +1,7 @@
+var o={
+    name:11,
+    age:12
+}
+var obj={}
+Object.assign(obj,o)
+console.log(obj);
---
+++ js/regexp/tets.js
@@ -0,0 +1,17 @@
+
+    let xieyuying={
+        love:""
+    }
+    let love=()=>{
+        if(xieyuying.love==='yujiashuang')
+        {
+            console.log(1)
+        }
+        else
+        {
+            xieyuying.love='yujiashuang'
+            console.log(xieyuying.love)
+        love()
+        }
+    }
+love()
