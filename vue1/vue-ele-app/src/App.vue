72cdabb9ff1a3d1d800300938000c72e38278a92
1131446340aa <1131446340@qq.com>
2019-11-02 18:22:09 +0800

    vue

--- vue1/vue-ele-app/src/App.vue
+++ vue1/vue-ele-app/src/App.vue
@@ -2,6 +2,17 @@
   <div id="app">
     <!-- 头部 -->
     <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+        <div class="tab-item">
+        <router-link to="/ratings">详情</router-link>
+      </div>
+        <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -31,6 +42,22 @@ export default {
 }
 </script>
 
-<style>
+<style lang="stylus">
+@import './common/stylus/mixin'
+.tab
+  display flex
+  height 40px
+  line-height 40px
+  width 100%
+  border-1px(rgba(7, 17, 27, 0.1))
+  &-item
+    flex 1
+    text-align center
+    a
+      text-decoration none
+      display block
+      color rgb(77,85,93)
+      &.router-link-active
+        color rgb(240,20,20)
 
 </style>
