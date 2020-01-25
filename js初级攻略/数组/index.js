0f43c47ac6ec6b00c75974574132034b11c03aa8
1131446340aa <1131446340@qq.com>
2020-01-25 21:20:04 +0800

    数组

---
+++ js初级攻略/数组/index.js
@@ -0,0 +1,27 @@
+// // var hege = ["Cecilie", "Lone"];
+// // var stale = ["Emil", "Tobias", "Linus"];
+// // var kai = ["Robin"];
+// // var children = hege.concat(stale,kai,"123");
+
+// // kai.push(123)
+// // console.log(children);
+// function a(x,y){
+//     // 
+//     // let m=Array.of(arguments).concat("123")
+// //    console.log(m[0][0] );
+// //    [...arguments].map(item=>{
+// //        console.log(item);
+       
+// //    })
+// let q=[123]
+// let m=q.concat(arguments)
+// console.log(m);
+
+   
+// }
+// a(1,2)
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.values());
+
+
+
