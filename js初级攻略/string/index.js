bf38a816c675091d72ed5cff1cdc5992cb8aeb4d
1131446340aa <1131446340@qq.com>
2020-01-26 16:21:52 +0800

    字符串总结

---
+++ js初级攻略/string/index.js
@@ -0,0 +1,7 @@
+// var str="The rain in SPAIN stays mainly in the plain"; 
+// var n=str.match(/ain/gi);
+// var str="Visit Microsoft! Visit Microsoft!";
+// var n=str.replace(/Microsoft/g,"Runoob");
+var str="Hello world!";
+var n=str.slice(1,3)
+console.log(n);
