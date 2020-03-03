4f544adc522361d83106c30f4464119d89c67bf9
1131446340aa <1131446340@qq.com>
2020-03-03 20:51:38 +0800

    修改bug

--- vue1/books/pachong/index1.js
+++ vue1/books/pachong/index1.js
@@ -6,34 +6,18 @@ let fs = require('fs')
 let FS = require('../06demo-fs/index.js')
 async function run() {
     let json = await FS.fsRead('./book.json')
-    json = await JSON.parse(json)
-    // console.log(json[1121]);
-    
+    json = await JSON.parse(json)  // 将json字符串转化为数组
     console.log(`总计${json.length}部小说等待下载......`);
-    // console.log(json[615]);
-    
     let brower = await puppeteer.launch({
-
-        // defaultViewport: {
-        //     width: 1440,
-        //     height: 800
-        // },
-        // timeout:1000000,
         headless: true
     })
-    let count = 5
-
+    let count = 0
     async function download(urls, title) {
-
-        // console.log(123);
-        // console.log(reg.exec(title));
         console.log(count);
-        
         let page = await brower.newPage()
         // await console.log(pages[i]);
         await page.setRequestInterception(true)
         await page.on('request', Interception => {
-
             let urlobj = url.parse(Interception.url())
             if (urlobj.hostname == 'googleads.g.doubleclick.net') {
                 Interception.abort()
@@ -41,43 +25,27 @@ async function run() {
                 Interception.continue()
             }
         })
-console.log(urls);
-
-        // await page.goto(urls, {
-        //     waitUntil: 'networkidle0'
-        // })
-        // // await page.waitForSelector('#table_files_wrapper tbody.even a')
-        // let html = await page.content()
-        // // console.log(html);
-
-        // let httpurl = await $('.table tbody .even a', html).attr('href')||urls
-        // console.log(httpurl);
-        if (urls) {
-           
-            
-            httpurl = urls
-            console.log(httpurl);
+        await page.goto(urls, {
+            waitUntil: 'networkidle0'
+        })
+        let html = await page.content()
+        let httpurl = await $('.table tbody .even a', html).attr('href')
+        if (httpurl) {
             await page.close()
-
             async function tolodownload() {
-
                 // await console.log(pages[i]);
-               
                 try {
-                     let page = await brower.newPage()
+                    let page = await brower.newPage()
                     await page.setRequestInterception(true)
                     page.on('request', Interception => {
-
                         let urlobj = url.parse(Interception.url())
                         //
-                        if (urlobj.hostname == '14804066.ch1.ctc.data.tv002.com' || urlobj.hostname == '14804066.169.ctc.data.tv002.com' || urlobj.hostname == '14804066.164.ctc.data.tv002.com'||urlobj.hostname == '14804066.142.ctc.data.tv002.com') {
+                        if (urlobj.hostname == '14804066.ch1.ctc.data.tv002.com' || urlobj.hostname == '14804066.169.ctc.data.tv002.com' || urlobj.hostname == '14804066.164.ctc.data.tv002.com' || urlobj.hostname == '14804066.142.ctc.data.tv002.com') {
+                            //拦截下载地址域名
                             let ws = fs.createWriteStream('./book/' + title + '.epub')
-                          
-                            console.log(urlobj.href);
-                            
                             axios.get(urlobj.href, { responseType: "stream" }).then(function (res) {
                                 console.log("正在下载:" + title);
-                                res.data.pipe(ws)
+                                res.data.pipe(ws) //通过管道流下载小说
                                 ws.on('close', function () {
                                     console.log("下载已完成:" + title);
                                     count = count + 1
@@ -88,68 +56,58 @@ console.log(urls);
                                         }
                                         catch (err) {
                                             console.log("失败,重新.");
-
                                             Run()
                                         }
-                                    }
+                                    }//当下载完毕之后通过递归形式再次进行下载
                                     if (count < json.length) {
                                         Run()
                                     }
                                 })
                             })
-
                             Interception.abort()
                         }
                         if (urlobj.hostname == 'googleads.g.doubleclick.net') {
                             Interception.abort()
                         }
-                        if (urlobj.hostname != 'googleads.g.doubleclick.net' && urlobj.hostname != '14804066.ch1.ctc.data.tv002.com' && urlobj.hostname != '14804066.169.ctc.data.tv002.com' && urlobj.hostname != '14804066.164.ctc.data.tv002.com'&&urlobj.hostname != '14804066.142.ctc.data.tv002.com') {
+                        if (urlobj.hostname != 'googleads.g.doubleclick.net' && urlobj.hostname != '14804066.ch1.ctc.data.tv002.com' && urlobj.hostname != '14804066.169.ctc.data.tv002.com' && urlobj.hostname != '14804066.164.ctc.data.tv002.com' && urlobj.hostname != '14804066.142.ctc.data.tv002.com') {
                             Interception.continue()
                         }
                     })
-
+                  httpurl= 'https://590m.com'+httpurl
                     await page.goto(httpurl, {
                         waitUntil: 'networkidle0'
                     })
                     let html = await page.content()
-                    // let $ = await cheerio.load(html)
                     let timer = await $('.display-4.fs-2.mb-3.mt-3.font-weight-normal.text-warning', html).text()
                     let reg1 = await RegExp(/小时/)
-                    let reg2 = await RegExp(/分钟/)
+                    let reg2 = await RegExp(/分钟/)                
                     let num = await timer.replace(/[^0-9]/ig, "") * 1;
-                  if(!timer){
-                      count=count+1
-                    function Run() {
-                        try {
-                            download(json[count].urls, json[count].title)
+                    if (!timer) {
+                        count = count + 1
+                        function Run() {
+                            try {
+                                download(json[count].urls, json[count].title)
+                            }
+                            catch (err) {
+                                console.log("失败,重新.");
+                                Run()
+                            }
                         }
-                        catch (err) {
-                            console.log("失败,重新.");
-        
+                        if (count < json.length) {
                             Run()
                         }
                     }
-                    if (count < json.length) {
-                        Run()
-                    }
-                      
-                  }
-                    
-                   else{
-                    if (reg1.exec(timer) || (reg2.exec(timer) && num > 10)) {
-                        FS.fsWrite('lost_book.json',`,{"title":"${title}"}`)
-                        count = await count + 1
-                        await download(json[count].urls, json[count].title)
-                    }
-
                     else {
-                        let btn = await page.$('.btn.btn-outline-secondary.fs--1')
-                        await btn.click()
+                        if (reg1.exec(timer) || (reg2.exec(timer) && num > 10)) {
+                            FS.fsWrite('lost_book.json', `,{"title":"${title}"}`)
+                            count = await count + 1
+                            await download(json[count].urls, json[count].title)
+                        }
+                        else {
+                            let btn = await page.$('.btn.btn-outline-secondary.fs--1')
+                            await btn.click()
+                        }
                     }
-                   }
-                    // await page.waitForSelector('#table_files_wrapper tbody.even a')
-                    // let html = await page.content()
-
                 }
                 catch (err) {
                     page.close()
@@ -157,7 +115,6 @@ console.log(urls);
                 }
             }
             tolodownload()
-
         }
         else {
             count = count + 1
@@ -167,7 +124,6 @@ console.log(urls);
                 }
                 catch (err) {
                     console.log("失败,重新.");
-
                     Run()
                 }
             }
@@ -175,9 +131,6 @@ console.log(urls);
                 Run()
             }
         }
-
-
-
     }
     async function run() {
         try {
@@ -185,7 +138,6 @@ console.log(urls);
         }
         catch (err) {
             console.log("失败,重新.");
-
             await run
         }
     }
